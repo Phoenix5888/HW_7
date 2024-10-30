738951dff45f05833aaa24c0f5ea01b5dac77972
Hanna Liubarska <gvg8say@gmail.com>
2024-10-30 22:08:22 +0200

    seven commit

--- file1.js
+++ file1.js
@@ -5,6 +5,8 @@ function reverseArray(arr) {
     // }
 //     return reversed;
 return arr.slice().reverse()
+
+// return [...arr].reverse()
 }
 const originalArray = [1, 2, 3, 4, 5];
 const reversedArray = reverseArray(originalArray);
